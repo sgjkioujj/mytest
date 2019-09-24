c4f2b51fe67286dd470f772a88b54e014746001f
sgjkioujj <sgjkioujj@163.com>
2019-09-24 21:46:24 +0800

    将Java工程添加版本库

---
+++ java/src/com/sgjk/Day1/Demo1_firstWorld.java
@@ -0,0 +1,20 @@
+package com.sgjk.Day1;
+
+public class Demo1_firstWorld {
+
+    public static void main(String[] args) {
+        int i = 1;
+        int a = i + 1;
+        System.out.println(i);
+        System.out.println(a);
+        System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("1111");
+        a();
+        System.out.println("1111");
+
+    }
+    public static void a() {
+        System.out.println("22222");
+    }
+}
---
+++ java/src/com/sgjk/Day1/servletDemo1.java
@@ -0,0 +1,21 @@
+package com.sgjk.Day1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/servletDemo1")
+public class servletDemo1 extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
